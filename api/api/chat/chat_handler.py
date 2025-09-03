e54d0e1f488f6c9160e0e930fac1ac2be0ce0a9a
Ian Wolloff <Ian.Wolloff@portsmouthcc.gov.uk>
2025-09-03 12:59:38 +0100

    changes for checking vip

--- api/api/chat/chat_handler.py
+++ api/api/chat/chat_handler.py
@@ -6,11 +6,14 @@ from langchain_core.prompts import ChatPromptTemplate
 from api.search.search_handler import SearchHandler
 import json
 from types import SimpleNamespace
-
+from openai import AzureOpenAI
 
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
-from azure.ai.agents.models import ListSortOrder
+from azure.ai.agents.models import ListSortOrder, FunctionTool, ToolSet
+from api.actions.get_username import is_vip
+from typing import Callable, Set, Any
+
 
 search_handler = SearchHandler()
 
@@ -21,11 +24,25 @@ class ChatHandler:
             azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"]
         )
 
+        user_functions: Set[Callable[...,Any]] = {
+            is_vip
+        }
+
+        functions = FunctionTool(user_functions)
+        toolset = ToolSet()
+        toolset.add(functions)
+        
+
         self.project = AIProjectClient(
             credential=DefaultAzureCredential(),
             endpoint=os.environ["AZURE_OPENAI_ENDPOINT"]+'api/projects/firstProject')
         
-        self.agent_master = self.project.agents.get_agent("asst_auBBqrhcSppyJ1wMJyY5qdmK")
+        self.agent_master = self.project.agents.get_agent("asst_T2rfXiGefNibf4qfFHMtFSYp")
+        self.project.agents.enable_auto_function_calls(toolset)
+        self.project.agents.update_agent(
+            agent_id=self.agent_master.id,
+            toolset=toolset
+        )
         self.thread = self.project.agents.threads.create()
     
     def trigger_api_post_request(self,url, payload):
